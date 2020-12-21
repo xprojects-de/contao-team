2be7945e6edbc3f009ba9bf3e9514a36667c065d
XProjects B.Hummel <infos@x-projects.de>
2020-12-21 19:22:19 +0100

    Increment version add icon and class

--- contao-team/src/Events/TeamAlpdeskFrontendViewListener.php
+++ contao-team/src/Events/TeamAlpdeskFrontendViewListener.php
@@ -8,10 +8,15 @@ use Alpdesk\AlpdeskFrontendediting\Events\AlpdeskFrontendeditingEventElement;
 
 class TeamAlpdeskFrontendViewListener {
 
+  private static $icon = '../../../system/themes/flexible/icons/user.svg';
+  private static $iconclass = 'tl_team_baritem';
+
   public function __invoke(AlpdeskFrontendeditingEventElement $event): void {
 
     if ($event->getElement()->type === 'xproject_team') {
       $event->getItem()->setValid(true);
+      $event->getItem()->setIcon(self::$icon);
+      $event->getItem()->setIconclass(self::$iconclass);
       $event->getItem()->setPath('do=xprojects_team');
       $event->getItem()->setLabel($GLOBALS['TL_LANG']['team_label']);
     }
