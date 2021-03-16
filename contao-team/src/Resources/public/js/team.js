ed8640847357cd4da80b8fe34a6151f1d0e30127
XProjects B.Hummel <infos@x-projects.de>
2021-03-16 08:07:06 +0100

    Bugfixes and migrate to IntelliJ

--- contao-team/src/Resources/public/js/team.js
+++ contao-team/src/Resources/public/js/team.js
@@ -1,71 +1,70 @@
-
 (function ($) {
-  $(document).ready(function () {
+    $(document).ready(function () {
 
-    $(".ce_xprojects_team_overview .item").each(function (index) {
-      var self = $(this);
-      $(this).find('div.textcontainer').hide();
-      if ('ontouchstart' in document.documentElement) {
+        $(".ce_xprojects_team_overview .item").each(function (index) {
+            var self = $(this);
+            $(this).find('div.textcontainer').hide();
+            if ('ontouchstart' in document.documentElement) {
 
-        $(this).on('click', function () {
+                $(this).on('click', function () {
 
-          var current = self.find('div.textcontainer');
-          $("div.textcontainer").each(function (index) {
-            if ($(this).attr('data-id') !== current.attr('data-id')) {
-              $(this).hide();
+                    var current = self.find('div.textcontainer');
+                    $("div.textcontainer").each(function (index) {
+                        if ($(this).attr('data-id') !== current.attr('data-id')) {
+                            $(this).hide();
+                        }
+                    });
+                    current.css('height', self.find('div.imagecontainer').first().height() + "px");
+                    current.fadeToggle(150);
+
+                });
+            } else {
+                $(this).hover(function () {
+                    self.find('div.textcontainer').css('height', self.find('div.imagecontainer').first().height() + "px");
+                    self.find('div.textcontainer').stop(true).fadeToggle(150);
+                });
             }
-          });
-          current.css('height', self.find('div.imagecontainer').first().height() + "px");
-          current.fadeToggle(150);
-
         });
-      } else {
-        $(this).hover(function () {
-          self.find('div.textcontainer').css('height', self.find('div.imagecontainer').first().height() + "px");
-          self.find('div.textcontainer').stop(true).fadeToggle(150);
-        });
-      }
-    });
 
-    $(".ce_xprojects_team_overview .tagselector").each(function () {
-      $(this).on('click', function () {
         $(".ce_xprojects_team_overview .tagselector").each(function () {
-          $(this).removeClass('active');
+            $(this).on('click', function () {
+                $(".ce_xprojects_team_overview .tagselector").each(function () {
+                    $(this).removeClass('active');
+                });
+                var current = $(this);
+                current.addClass('active');
+                var tag = current.attr('data-tag');
+                $(".ce_xprojects_team_overview .item").each(function () {
+                    var tagsItems = $(this).attr('data-tags');
+                    if (tagsItems.includes(tag) || tag === '-') {
+                        $(this).show();
+                    } else {
+                        $(this).hide();
+                    }
+                });
+            });
         });
-        var current = $(this);
-        current.addClass('active');
-        var tag = current.attr('data-tag');
-        $(".ce_xprojects_team_overview .item").each(function () {
-          var tagsItems = $(this).attr('data-tags');
-          if (tagsItems.includes(tag) || tag === '-') {
-            $(this).show();
-          } else {
-            $(this).hide();
-          }
+
+        $("input.searchcontainer_inputtext").each(function () {
+            $(this).on('keyup', function () {
+                var valSearch = $(this).val().toLowerCase();
+                $(".ce_xprojects_team_overview .item").each(function () {
+                    var searchItems = $(this).attr('data-search');
+                    if (searchItems.toLowerCase().includes(valSearch) || valSearch.length < 2 || valSearch === '') {
+                        $(this).show();
+                    } else {
+                        $(this).hide();
+                    }
+                });
+            });
         });
-      });
-    });
 
-    $("input.searchcontainer_inputtext").each(function () {
-      $(this).on('keyup', function () {
-        var valSearch = $(this).val();
-        $(".ce_xprojects_team_overview .item").each(function () {
-          var searchItems = $(this).attr('data-search');
-          if (searchItems.includes(valSearch) || valSearch.length < 2 || valSearch === '') {
-            $(this).show();
-          } else {
-            $(this).hide();
-          }
+        $('.ce_xprojects_team_overview .lazy').lazy({
+            effect: "fadeIn",
+            effectTime: 1250,
+            threshold: 0
         });
-      });
-    });
 
-    $('.ce_xprojects_team_overview .lazy').lazy({
-      effect: "fadeIn",
-      effectTime: 1250,
-      threshold: 0
     });
-
-  });
 })(jQuery);
 
